noisy_img is corrected by online computation

main.py
@@ -13,6 +13,7 @@ from models.diffusion import DDPM
 from models.classifier import *
 from utilities.sampler import sampler,  DDIM_inversion, DDIM_generation
 from utilities.training import *
+from utilities.test import *
 from utilities.load_data import *
 
 
@@ -112,12 +113,38 @@ def main():
 
     # convertImgtoNoise(model,config)  ################  prepare training data by adding noise
 
-    d = FaceDataset("./data/training")
-    dataloader = DataLoader(d, batch_size=2,
-                        shuffle=True, num_workers=1)
     
     
-    train(model, config, dataloader)
+    
+    #############  TRAIN ############################
+    # d = FaceDataset("./data/training")
+    # dataloader = DataLoader(d, batch_size=2,
+    #                     shuffle=True, num_workers=1)
+    
+    
+    # train(model, config, dataloader)
+
+
+    ################  TEST  ##########################
+
+    
+    
+    torch.cuda.empty_cache() 
+
+    
+    model_pretrained = DDPM(config)
+
+
+ 
+
+
+    
+    for param in model.parameters():
+        param.requires_grad = False
+
+    xt = convertImgtoNoise(model,config, "test")
+    
+    test(model,config,xt)
         
     return 0
 

utilities/load_data.py
@@ -9,10 +9,18 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
 # get the path/directory
-def convertImgtoNoise(model,config):
-    folder_dir = config.training.clean_address
+def convertImgtoNoise(model,config, condition="train"):
+    if condition == "train":
+        clean_image_path = config.training.clean_address
+        noisy_image_path = config.training.noisy_address
+    if condition == "test":
+        clean_image_path = config.test.clean_address
+        noisy_image_path = config.test.noisy_address
+
+
+    folder_dir = clean_image_path
     image_list = os.listdir(folder_dir)
-    print(image_list)
+    
     for i in range(len(image_list)):
         # Read a PIL image
         image = Image.open(folder_dir +"/"+image_list[i])
@@ -37,11 +45,11 @@ def convertImgtoNoise(model,config):
         ims = ims.squeeze(0)    
         img = transforms.ToPILImage()(ims)
 
-        if not os.path.exists(config.training.noisy_address):
-            os.mkdir(config.output.address)    
-        img.save(os.path.join(config.training.noisy_address, image_list[i]))
+        if not os.path.exists(noisy_image_path):
+            os.mkdir(noisy_image_path)    
+        img.save(os.path.join(noisy_image_path, image_list[i]))
         img.close()
-
+    return xt
 class FaceDataset(Dataset):
     """source image, noisy image and target image."""
 
@@ -69,7 +77,7 @@ class FaceDataset(Dataset):
         img_name = os.path.join(self.root_dir,'clean', str(idx+1)+'.jpg')
 
         
-        noisy_name = os.path.join(self.root_dir,'noisy', str(idx+1)+'.jpg')
+        # noisy_name = os.path.join(self.root_dir,'noisy', str(idx+1)+'.jpg')
 
         label_name = os.path.join(self.root_dir,'clean', str(idx+1)+'.jpg')
         
@@ -88,17 +96,17 @@ class FaceDataset(Dataset):
         img = tr(x0)
 
 
-        noisy = Image.open(noisy_name)
-        transform = transforms.Compose([
-            transforms.PILToTensor(),
-            transforms.Resize((256,256))        
-        ]) 
-        x0 = transform(noisy)
-        # x0 = x0.unsqueeze(0).float()
-        x_min, x_max = x0.min(), x0.max()
-        x0 = (x0 - x_min) / (x_max-x_min)
-        tr = transforms.Compose([transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        noisy = tr(x0)
+        # noisy = Image.open(noisy_name)
+        # transform = transforms.Compose([
+        #     transforms.PILToTensor(),
+        #     transforms.Resize((256,256))        
+        # ]) 
+        # x0 = transform(noisy)
+        # # x0 = x0.unsqueeze(0).float()
+        # x_min, x_max = x0.min(), x0.max()
+        # x0 = (x0 - x_min) / (x_max-x_min)
+        # tr = transforms.Compose([transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        # noisy = tr(x0)
 
 
         label = Image.open(label_name)
@@ -116,4 +124,4 @@ class FaceDataset(Dataset):
 
 
 
-        return img,noisy,label
+        return img,label

utilities/sampler.py
@@ -101,31 +101,38 @@ def DDIM_inversion(model, config, x0):
 
 
 def DDIM_generation(model, config, xt):
-  
+   
+
     scheduler = LinearNoiseSchedulerDDIM(num_timesteps=config.samplingDDIM.num_timesteps,
                                     beta_start=config.samplingDDIM.beta_start,
                                     beta_end=config.samplingDDIM.beta_end)
     step = config.samplingDDIM.step_backward
-    t = 0
-    for i in tqdm(reversed(range(int(config.samplingDDIM.forward_timesteps/step)))):        
-        # xt.requiers_grad = False
+    for i in tqdm(reversed(range(1,int(config.samplingDDIM.forward_timesteps/step)))):        
         # Get prediction of noise
         noise_pred = model(xt, torch.as_tensor(i*step).unsqueeze(0).to(config.device))        
         # Use scheduler to get x0 and xt-1
         xt_1, x0_pred = scheduler.sample_prev_timestep(xt, noise_pred, torch.as_tensor(i).to(config.device), config.samplingDDIM.sigma, step)
         xt = xt_1
-        t = i
-    
-    
 
 
 
 
+
+        # ims = torch.clamp(xt, -1., 1.).detach().cpu()
+        # ims = (ims + 1) / 2
+        # ims = ims.squeeze(0)    
+        # img = torchvision.transforms.ToPILImage()(ims)
+
+
+
+
+
+        # if not os.path.exists(config.test.generated_address):
+        #     os.mkdir(config.test.generated_address)    
+        # img.save(os.path.join(config.test.generated_address, str(i)+".jpg"))
+        # img.close()
     
    
-    # noise_pred = model(xt, torch.as_tensor((t+1)*step).unsqueeze(0).to(config.device))
-    # xt_1, x0_pred = scheduler.sample_prev_timestep(xt, noise_pred, torch.as_tensor(t).to(config.device), config.samplingDDIM.sigma, step)
-    # xt = xt_1
     
     return xt
 

utilities/training.py
@@ -7,6 +7,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 from scheduler.linear_noise_scheduler import LinearNoiseScheduler,LinearNoiseSchedulerDDIM
 from tqdm import tqdm
+# from models.diffusion import DDPM
 
 def train(model, config, dataloader):
     torch.cuda.empty_cache() 
@@ -31,13 +32,14 @@ def train(model, config, dataloader):
         batch_size = 0
         total_loss = [0]*int(config.samplingDDIM.forward_timesteps/step)
 
-        for clean_img, noisy_img, label in dataloader:
+        for clean_img,  label in dataloader:
             batch_size += clean_img.size(0) 
             # clean_img = clean_img.to(config.device)  ## later it can be used for id loss but in this version we do not need this 
+            noisy_img = DDIM_inversion(model, config, clean_img)
             noisy_img = noisy_img.to(config.device)
             label = label.to(config.device)            
             
-            for i in tqdm(reversed(range(int(config.samplingDDIM.forward_timesteps/step)))):      
+            for i in tqdm(reversed(1,range(int(config.samplingDDIM.forward_timesteps/step)))):      
 
 
                 # Get prediction of noise
@@ -53,11 +55,16 @@ def train(model, config, dataloader):
                 l.backward()
                 optimizer.step()
                 total_loss[i] = l.data
-                noisy_img = torch.tensor(noisy_img)
+                noisy_img = noisy_img.clone().detach().requires_grad_(False)
             
             
         print("Epoch %d: SGD lr=%.4f"% (epoch, optimizer.param_groups[0]["lr"]))
-        print("total loss:", torch.tensor(total_loss)/batch_size)
+        print("total loss:", torch.mean(torch.tensor(total_loss)/batch_size).data)    
+        torch.save(model.state_dict(), config.training.checkpoints+"/ckpt"+str(epoch))
+    
+
+
+    
  
 
 